e0c65cfa04e90b6c5f4d4cfda36107fc7da6b32e
Marek Fewtrell <Marek_Fewtrell@outlook.com.au>
2018-01-01 20:46:18 +1000

    Added Pug template engine. Created basic template for site and included to each page.

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 
+const path = require("path")
 const express = require('express')
+//const pug = require('pug')
 
 const app = express()
 
-app.use(express.static('public'))
+app.set("view engine", "pug")
+app.set('views', './views')
+
+//app.use(express.static('./public'))
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/', function (req, res) {
-  return res.sendFile(process.cwd() + '/views/index.html')
+  //return res.sendFile(process.cwd() + '/views/index.html')
   //res.send("hello world")
+  res.render("index")
 })
 
 app.post('/', function (req, res) {
@@ -21,5 +28,23 @@ app.put('/', function (req, res) {
 app.delete('/', function (req, res) {
   res.send("got a DELETE request")
 })
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', {err: err});
+});
 
 app.listen(23200, ()=> console.log("app listenting on port 23200."))
