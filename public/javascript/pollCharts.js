a066f3ecb26c8c7ff2dfb693175d21f4a87f6e52
Marek Fewtrell <Marek_Fewtrell@outlook.com.au>
2018-01-15 18:03:45 +1000

    Implemented simple random colours for the chart to display.

--- public/javascript/pollCharts.js
+++ public/javascript/pollCharts.js
@@ -4,17 +4,20 @@ window.onload = function() {
 
   var pollLabels = []
   var pollDataSet = []
+  var backgroundColour = []
 
   var pollOptions = pollData.pollOptions
 
   for (var i = 0 ; i < pollOptions.length; i++) {
     pollLabels.push(pollOptions[i].hasOwnProperty('name') ? pollOptions[i].name : "unnamed")
     pollDataSet.push(pollOptions[i].count)
+    backgroundColour.push(createRandomColour())
   }
 
   var data = {
     datasets: [{
-      data: pollDataSet
+      data: pollDataSet,
+      backgroundColor: backgroundColour
     }],
 
     labels: pollLabels
@@ -30,41 +33,9 @@ window.onload = function() {
   })
 };
 
-
-
-/*var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});*/
+function createRandomColour() {
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  return "rgba(" + r + "," + g + "," + b + ", 0.2)";
+}
